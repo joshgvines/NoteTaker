8cacbb60c5ebf3172538b69db43ed0eef283ab8a
joshgvines <joshgvines@gmail.com>
2022-10-19 23:43:43 -0500

    feature/04 Adding EditMenu and delete menu item stub function.

---
+++ src/main/java/com/notetaker/ui/menu/EditMenu.java
@@ -0,0 +1,49 @@
+package com.notetaker.ui.menu;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class EditMenu {
+
+    private static final String MENU_NAME = "Edit";
+
+    private static JMenu editMenu;
+    private static JMenuItem deleteItem;
+
+    public EditMenu() {
+        initialize();
+    }
+
+    private static void initialize() {
+        editMenu = new JMenu(MENU_NAME);
+        editMenu.setMnemonic(KeyEvent.VK_E);
+        load();
+    }
+
+    protected static void load() {
+        try {
+            deleteItem = new JMenuItem("Delete");
+
+            deleteItem.addActionListener(deleteFileAction());
+
+            editMenu.add(deleteItem);
+        } finally {
+            editMenu.revalidate();
+        }
+    }
+
+    private static ActionListener deleteFileAction() {
+        deleteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
+        return e -> System.out.println("delete");
+    }
+
+    protected static synchronized JMenu getInstance() {
+        if (editMenu == null) {
+            initialize();
+        }
+        return editMenu;
+    }
+
+}
